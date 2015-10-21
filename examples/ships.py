1894220485e0a025f181537ec164a73c71fcc575
Phil Howard <phil@gadgetoid.com>
2015-10-21 21:09:09 +0000

    New ships example, whee!

--- examples/ships.py
+++ examples/ships.py
@@ -111,6 +111,8 @@ class Ship(Widget):
 
     def __init__(self, color, position, velocity):
         self.vx, self.vy = velocity
+        self.drag = 0.95
+        self.thrust = False
         self.color = color
 
         super(Ship, self).__init__(position, (0, 0))
@@ -124,6 +126,14 @@ class Ship(Widget):
     def update(self):
         self.x = int(self.x + self.vx)
         self.y = int(self.y + self.vy)
+    
+        if not self.thrust:
+            self.vx *= self.drag
+            if abs(self.vx) <= 0.001:
+                self.vx = 0
+            self.vy *= self.drag
+            if abs(self.vy) <= 0.001:
+                self.vy = 0
 
         if self.x >= 800:
             self.x = 799
@@ -139,6 +149,16 @@ class Ship(Widget):
 
     def render(self, screen):
         pygame.draw.circle(screen, self.color, self.position, 10, 0)
+
+        text = self.font.render("v:{},{}".format(
+            round(self.vx*10)/10.0,
+            round(self.vy*10)/10),
+             1, self.color)
+        textpos = text.get_rect()
+        textpos.centerx = self.x
+        textpos.centery = self.y + 10
+        screen.blit(text, textpos)
+
         
 
 class Dial(Widget):
@@ -168,13 +188,14 @@ class Dial(Widget):
 
         x, y = touch.position
 
-        self.distance = math.hypot(x - self.x, y - self.y) / self.radius
+        self.distance = math.hypot(x - self.x, y - self.y) / float(self.radius)
+        
+        #self.distance = math.sqrt((x - self.x)**2 + (y - self.y)**2) / float(self.radius)
 
         dx = x - self.x
         dy = y - self.y
     
         self.value = math.degrees(math.atan2(dy, dx) % (2*math.pi)) / 360.0
-
 
         if callable(self._value_changed):
             self._value_changed(self.value * 2 * math.pi, self.distance)
@@ -182,7 +203,9 @@ class Dial(Widget):
 
     def touch_inside(self, touch):
         x, y = touch.position
-        distance = math.hypot(x - self.x, y - self.y)
+        #distance = math.hypot(x - self.x, y - self.y)
+
+        distance = math.sqrt((x - self.x)**2 + (y - self.y)**2)
 
         if distance <= self.radius:
             return True
@@ -209,6 +232,7 @@ class Dial(Widget):
         text = self.font.render(str(int(self.distance * 100)) + '%', 1, self.color)
         textpos = text.get_rect()
         textpos.centerx, textpos.centery = handle_pos
+        textpos.centery += 20
         screen.blit(text, textpos)
 
 class Slider(Widget):
@@ -331,63 +355,32 @@ def button_event(b, e, t):
 def update_ship_velocity(ship_idx, angle, velocity):
     global ship
     s = ship[ship_idx]
-    if s is not None:
-        velocity = min(1,velocity)
+    velocity = min(1,velocity)
+
+    if velocity == 0:
+        s.thrust = False
+
+    if s is not None and velocity > 0.1:
+        s.thrust = True
         s.vx = velocity * 10 * math.cos(angle)
         s.vy = velocity * 10 * math.sin(angle)
 
 Dial(
     (0, 100),
-    (255, 0, 0),
-    (100, 380),
+    (255, 0, 255),
+    (100, 240),
     80,
     lambda v, a: update_ship_velocity(0, v, a))
 
 Dial(
     (0, 100),
-    (255, 0, 0),
-    (700, 380),
+    (255, 255, 0),
+    (700, 240),
     80,
     lambda v, a: update_ship_velocity(1, v, a))
 
-Button(
-    "Quack!",
-    (255, 0, 0),
-    (20, 20),
-    (160, 60), 
-    button_event)
-
-Button(
-    "Duck!",
-    (0,255,0), 
-    (20, 100),
-    (160, 60), 
-    button_event)
-
-Button(
-    "Moo!",
-    (0, 0, 255),
-    (20, 180),
-    (160, 60),
-    button_event)
-
-for x in range(6):
-    Slider(
-        (0,100),
-        (255, 255, 0),
-        (205 + (x*70), 20),
-        (40, 390),
-        None)
-
-Slider(
-    (0,100),
-    (0, 255, 255),
-    (205,430),
-    (390,40),
-    None)
-
 ship = [
-    Ship((0, 255, 255),(400, 240),(0, 0)),
+    Ship((255, 0, 255),(400, 240),(0, 0)),
     Ship((255, 255, 0),(400, 250),(0, 0))]
 
 ts.run()
