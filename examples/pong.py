2bb8605b017eeb67570305c099871ebe7a316a6e
Phil Howard <phil@gadgetoid.com>
2015-10-23 21:13:26 +0000

    Working pong, yay

--- examples/pong.py
+++ examples/pong.py
@@ -1,4 +1,5 @@
 import sys
+import random
 import pygame
 import time
 import os
@@ -7,6 +8,7 @@ import ft5406
 from gui import text
 from collections import namedtuple
 import math
+from turtle import Vec2D
 
 pygame.init()
 
@@ -23,23 +25,65 @@ Size = namedtuple('Size', 'w h')
 def millis():
     return int(round(time.time() * 1000))
 
+def text(screen, text, position, size, color):
+    font = pygame.font.SysFont("droidsansmono", size)
+    text = font.render(text, 1, color)
+    textpos = text.get_rect()
+    textpos.centerx, textpos.centery = position
+    screen.blit(text, textpos)
+
 class Ball():
     def __init__(self):
         
         global width, height
 
-        self.speed = 0.3
-        self.angle = 0
+        self.position = Vec2D(width/2, height/2)
 
-        self.x = width/2
-        self.y = height/2
-
-        self.vx = self.speed*math.cos(self.angle)
-        self.vy = self.speed*math.sin(self.angle)
+        self.velocity = Vec2D(0.3,0.3)
 
         self.radius = 10
 
         self.color = (255, 255, 255)
+
+    def reset(self):
+        self.velocity = Vec2D(0.3,0.3).rotate(random.randint(0, 360))
+        self.position = Vec2D(width/2, height/2)
+
+    @property
+    def x(self):
+        return self.position[0]
+
+    @x.setter
+    def x(self, value):
+        self.position = Vec2D(value, self.position[1])
+    
+    @property
+    def y(self):
+        return self.position[1]
+
+    @y.setter
+    def y(self, value):
+        self.position = Vec2D(self.position[0], value)
+
+    @property
+    def vx(self):
+        return self.velocity[0]
+
+    @vx.setter
+    def vx(self, value):
+        self.velocity = Vec2D(value, self.velocity[1])
+
+    @property
+    def vy(self):
+        return self.velocity[1]
+
+    @vy.setter
+    def vy(self, value):
+        self.velocity = Vec2D(self.velocity[0], value)
+
+    @property
+    def speed(self):
+        return abs(self.velocity)
 
     def intersects(self, rect):
         rx, ry = rect.center
@@ -61,44 +105,50 @@ class Ball():
 
         return c_sq <= self.radius**2.0
 
-    def update(self, delta, collide):
+    def update(self, delta, left_player, right_player):
         global width
 
-        self.x += self.vx * delta
-        self.y += self.vy * delta
+        self.position += self.velocity * delta
 
-        for item in collide:
-            if self.intersects(item):
-                cx, cy = item.center
-                w, h = item.size
+        if (self.x < 100 and self.vx < 0) or (self.x > width - 100 and self.vx > 0):
+            for item in [left_player, right_player]:
+                if self.intersects(item):
+                    item.success()
 
-                relative_y = (cy - self.y) / (h/w)
+                    cx, cy = item.center
+                    w, h = item.size
+                    relative_y = (cy - self.y) / (h / 2)
 
-                angle = relative_y * 5*(math.pi/12)
+                    speed = self.speed + (abs(relative_y)/4)
 
-                self.vx = self.speed*math.cos(angle)
-                self.vy = self.speed*math.sin(angle)
+                    angle = relative_y * 5 * (math.pi / 12)
+                    
+                    if self.x > width/2:
+                        self.x = item.position.x - self.radius 
+                        self.velocity = Vec2D(
+                            speed * -math.cos(angle),
+                            speed * -math.sin(angle))
+                    else:
+                        self.x = item.position.x + item.width + self.radius
+                        self.velocity = Vec2D(
+                            speed * math.cos(angle),
+                            speed * -math.sin(angle))
 
 
-        if self.x - self.radius <= 0:
-            self.x = self.radius
-            self.vx *= -1
+        if self.x - self.radius < 0 and self.vx < 0:
+            left_player.fail()
+            self.reset()
+        elif self.x + self.radius > width and self.vx > 0:
+            right_player.fail()
+            self.reset()
 
-        if self.y - self.radius <=0:
+        if self.y - self.radius < 0 and self.vy < 0:
             self.y = self.radius
             self.vy *= -1
-
-        if self.y + self.radius >= height:
+        elif self.y + self.radius > height and self.vy > 0:
             self.y = height - self.radius
             self.vy *= -1
 
-        if self.x + self.radius >= width:
-            self.x = width - self.radius
-            self.vx *= -1
-
-    @property
-    def position(self):
-        return Position(x=self.x, y=self.y)
 
     def render(self, screen):
         pygame.draw.circle(
@@ -113,7 +163,8 @@ class Player():
     def __init__(self, side):
 
         global width, height
-
+        
+        self.score = 0
         self.y = height/2
         self.next_y = self.y
 
@@ -122,11 +173,17 @@ class Player():
         else:
             self.x = width - 50
         
-        self.width = 20
-        self.height = 50
+        self.width = 10
+        self.height = 100
 
     def paddle(self, y):
         self.next_y = y
+
+    def success(self):
+        self.score += 1
+
+    def fail(self):
+        self.score -= 1
 
     @property
     def center(self):
@@ -182,11 +239,9 @@ while True:
 
         x, y = touch.position
 
-        print("Got touch at {},{}".format(x, y))
-
-        if x < 50:
+        if x < touch_zone:
             player_one.paddle(y)
-        elif x > width-50:
+        elif x > width-touch_zone:
             player_two.paddle(y)
                     
 
@@ -198,10 +253,21 @@ while True:
     screen.fill((0, 0, 0))
 
     text(screen, "Hit ESC to exit!", (400, 460), 30, (255, 0, 0))  
+
+    pygame.draw.rect(
+            screen,
+            (64, 64, 64),
+            (((width/2) - 2, 40),
+            (4,height-80)),
+            0
+        )
+
+    text(screen, "{0:02d}".format(player_one.score), (50, 50), 30, (255, 255, 255))
+    text(screen, "{0:02d}".format(player_two.score), (width-50, 50), 30, (255, 255, 255))
     
     player_one.update()
     player_two.update()
-    ball.update(time_delta,[player_one, player_two])
+    ball.update(time_delta, player_one, player_two)
 
     ball.render(screen)
     player_one.render(screen)
